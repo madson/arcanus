6caa15cbf7fca5ede05181dc441b4af56d5f529d
Lenon Marcel <lenon.marcel@gmail.com>
2013-02-01 18:24:09 -0200

    Refactor AuthorService to use Digest.hash and Digest.kdf methods.

--- lib/services/author_service.rb
+++ lib/services/author_service.rb
@@ -4,39 +4,21 @@ class AuthorService
   end
 
   def save
-    if @author.valid?
-      hash_password
-      @author.save
-    else
-      false
-    end
+    @author.valid? && hash_password && @author.save
   end
 
   def valid_password?(password)
     salt, hash = @author.hashed_password.unpack("a64 a*")
-    hash === kdf(hash(password), salt)
+    hash === Digest.kdf(Digest.hash(password), salt)
   end
 
   private
 
   def hash_password
     salt = OpenSSL::Random.random_bytes(64)
-    hash = kdf(hash(@author.password), salt)
+    hash = Digest.kdf(Digest.hash(@author.password), salt)
 
     @author.hashed_password = [salt, hash].pack("a64 a*")
-  end
-
-  def hash(password)
-    OpenSSL::Digest::SHA512.digest(password)
-  end
-
-  def kdf(password, salt)
-    OpenSSL::PKCS5::pbkdf2_hmac(
-      password,
-      salt,
-      Setting.pbkdf_iterations,
-      512,
-      OpenSSL::Digest::SHA512.new
-    )
+    true
   end
 end
--- spec/services/author_service_spec.rb
+++ spec/services/author_service_spec.rb
@@ -2,16 +2,14 @@ require "spec_helper"
 
 describe AuthorService do
   let(:author) { double }
-  let(:salt) { "A" * 64 }
   let(:password) { "123456" }
-  let(:hashed_password) { IO.binread("spec/fixtures/bin/hashed_password") }
+  let(:salt) { "A" * 64 }
+  let(:hash) { "<hashed password>" }
+  let(:kdf) { "<kdf password>" }
+  let(:hashed_password) { salt + kdf }
 
+  before { author.stub(password: password, hashed_password: hashed_password) }
   subject { AuthorService.new(author) }
-
-  before do
-    Setting.stub(pbkdf_iterations: 1)
-    author.stub(password: password)
-  end
 
   describe ".new" do
     it "receives an Author object" do
@@ -25,11 +23,14 @@ describe AuthorService do
       expect(subject.save).to be_false
     end
 
-    it "creates a new hashed password and then saves the author" do
+    it "hashes password and then saves author" do
       OpenSSL::Random.stub(random_bytes: salt)
 
+      Digest.should_receive(:hash).with(password).and_return(hash)
+      Digest.should_receive(:kdf).with(hash, salt).and_return(kdf)
+
       author.stub(valid?: true)
-      author.should_receive(:hashed_password=).with(hashed_password)
+      author.should_receive(:hashed_password=).with(salt + kdf)
       author.should_receive(:save).and_return(true)
 
       expect(subject.save).to be_true
@@ -37,14 +38,20 @@ describe AuthorService do
   end
 
   describe "#valid_password?" do
-    before { author.stub(hashed_password: hashed_password) }
-
     it "returns true when given password is valid" do
+      Digest.should_receive(:hash).with(password).and_return(hash)
+      Digest.should_receive(:kdf).with(hash, salt).and_return(kdf)
+
       expect(subject.valid_password?(password)).to be_true
     end
 
     it "returns false when given password is wrong" do
-      expect(subject.valid_password?("wrong password")).to be_false
+      password = "wrong password"
+
+      Digest.should_receive(:hash).with(password).and_return(hash)
+      Digest.should_receive(:kdf).with(hash, salt).and_return("<wrong kdf>")
+
+      expect(subject.valid_password?(password)).to be_false
     end
   end
 end
